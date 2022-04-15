63608fb386b40e915c90c4f0231826637e636fc8
Faisal Ibrahim <faisal.im048@gmail.com>
2022-04-15 02:23:15 +0200

    Update SingleResponsibilityPrincipleExmaple.php

--- Single-responsibility Principle/SingleResponsibilityPrincipleExmaple.php
+++ Single-responsibility Principle/SingleResponsibilityPrincipleExmaple.php
@@ -4,20 +4,17 @@ namespace Student\Attendance;
 
 class Attendance
 {
-
 }
 
 namespace Student\GradeCalculator;
 
 class GradeCalculator
 {
-
 }
 
 namespace Student\Payments;
 
 class Payments
 {
-
 }
 
